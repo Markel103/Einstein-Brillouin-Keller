b669848b996a94f7775d21af608c45616876f5a1
albahc01 <102954392+albahc01@users.noreply.github.com>
2022-04-06 09:06:08 +0200

    Add files via upload

---
+++ konstanteak.f90
@@ -0,0 +1,8 @@
+module konstanteak
+use mcf_tipos
+real(kind=dp),parameter,public:: pi=acos(-1.0_dp)
+real(kind=dp),parameter,public:: hbar=1.054571817E-34_dp
+real(kind=dp),parameter,public
+
+
+end module konstanteak
