4007f11b55267f6ca8d7c552acc3f0d112aa52d6
albahc01 <102954392+albahc01@users.noreply.github.com>
2022-04-07 17:01:52 +0200

    Add files via upload

--- funtzioak.f90
+++ funtzioak.f90
@@ -2,16 +2,30 @@ module funtzioak
 public:: U
 
 contains
-function U(r)
+!function U612(r)
+!use mcf_tipos
+!real(kind=dp),intent(in)::r
+!real(kind=dp)::U612,a,b
+!real(kind=dp),parameter:: eps=0.0019636_dp  !a=4.96E7_dp !atomic units
+!real(kind=dp),parameter:: r0=7.36_dp ! b=624.0_dp !atomic units
+!a=eps*r0**12.0_dp
+!b=2.0_dp*eps*r0**6.0_dp
+!U612=a/(r**12.0_dp)-b/(r**6.0_dp) ! potentzial 6-12
+!end function U612
+
+function Vr612(r)
+use konstanteak
 use mcf_tipos
 real(kind=dp),intent(in)::r
-real(kind=dp)::U,a,b
+real(kind=dp)::Vr612,a,b,U612
 real(kind=dp),parameter:: eps=0.0019636_dp  !a=4.96E7_dp !atomic units
 real(kind=dp),parameter:: r0=7.36_dp ! b=624.0_dp !atomic units
 a=eps*r0**12.0_dp
 b=2.0_dp*eps*r0**6.0_dp
-U=a/(r**12.0_dp)-b/(r**6.0_dp) ! potentzial 6-12
-end function U
+U612=a/(r**12.0_dp)-b/(r**6.0_dp) ! potentzial 6-12
+Vr612=real(l,dp)*(real(l,dp)+1)*hbar/(2.0_dp*mu*r**2.0_dp) + U612
+print*, r, U612,Vr612
+end function Vr612
 
 
 end module funtzioak
--- konstanteak.f90
+++ konstanteak.f90
@@ -1,8 +1,12 @@
 module konstanteak
 use mcf_tipos
 real(kind=dp),parameter,public:: pi=acos(-1.0_dp)
+real(kind=dp),parameter,public:: hbar_atomiko=1.0_dp
 real(kind=dp),parameter,public:: hbar=1.054571817E-34_dp
-real(kind=dp),parameter,public
+integer,parameter,public:: l=1
+real(kind=dp),parameter,public:: m1=1.0_dp
+real(kind=dp),parameter,public:: m2=1.0_dp
+real(kind=dp),parameter,public:: mu=m1*m2/(m1+m2)
 
 
 end module konstanteak
