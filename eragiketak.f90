4b0a89c7f66ddd84ed56a8ab8866a1d2cd96a02a
Markel103 <67502202+Markel103@users.noreply.github.com>
2022-04-27 15:39:59 +0200

    Add files via upload

--- eragiketak.f90
+++ eragiketak.f90
@@ -1,6 +1,6 @@
 module eragiketak
 use mcf_tipos
-public:: deribatuV,minimoV,erroak,pr,integral_pr
+public:: deribatuV,minimoV,erroak,pr,integral_pr,energia_bilaketa
 
 contains
 
@@ -24,7 +24,7 @@ function minimoV(V,a0,b0)
 use mcf_tipos
 real(kind=dp),intent(in)::a0,b0
 real(kind=dp):: minimoV,erdia,a2,b2
-real(kind=dp),parameter:: eps=1.0E-14_dp
+real(kind=dp),parameter:: eps=1.0E-13_dp
 integer::i
 integer,parameter:: nmax=10000
 interface
@@ -55,7 +55,7 @@ use mcf_tipos
 real(kind=dp),intent(in)::a0,b0,E
 real(kind=dp):: erdia,a1,b1,a2,b2,min
 real(kind=dp),dimension(2)::erroak
-real(kind=dp),parameter:: eps=1.0E-14_dp
+real(kind=dp),parameter:: eps=1.0E-13_dp
 integer::i,j
 integer,parameter:: nmax=10000
 interface
@@ -126,29 +126,75 @@ end function pr
 function integral_pr(V,a0,b0,E)
 use mcf_tipos
 use konstanteak
-use mcf_cuadratura
 real(kind=dp),intent(in)::a0,b0,E
 real(kind=dp),dimension(2)::r1r2
-real(kind=dp)::integral_pr,emaitza
-real(kind=dp),parameter:: eps=1.0E-14_dp
+real(kind=dp)::integral_pr
+real(kind=dp)::r1,r2,h,hh
+integer::nn,j
 interface
 function V(r)
 use mcf_tipos
 real(kind=dp),intent(in)::r
 real(kind=dp)::V
-end function
+end function V
 end interface
 
 r1r2=erroak(V,a0,b0,E)
 r1=r1r2(1)
 r2=r1r2(2)
 
-call romberg(pr,r1,r2,emaitza,eps) ! en romberg pr solo puede tener un argumento
-integral_pr=emaitza
+h=r2-r1
+nn=10000
+hh=h/nn
+
+
+integral_pr=0.0_dp
+do j=0,(nn-1)
+integral_pr=integral_pr+(hh/2.0_dp)*((pr(V,E,r1+hh*real(j,dp)))+pr(V,E,r1+hh*real(j+1,dp)))
+enddo
 
 end function integral_pr
 
+function energia_bilaketa(V,a0,b0,nr)
+use mcf_tipos
+use konstanteak
+real(kind=dp),intent(in)::a0,b0
+real(kind=dp),dimension(2)::r1r2
+real(kind=dp)::emaitza,EE,a,b,energia_bilaketa
+real(kind=dp),parameter::eps=1.0E-13_dp
+integer::i
+integer,intent(in)::nr
+integer,parameter::imax=1000
+interface
+function V(r)
+use mcf_tipos
+real(kind=dp),intent(in)::r
+real(kind=dp)::V
+end function V
+end interface
 
+a=V(minimoV(V,a0,b0))
+b=0.0_dp
+!print*,a
+!print*,b
+
+do i=1,imax
+if (abs(b-a)<eps) then
+exit
+endif
+
+EE=(b+a)/2.0_dp
+!print*,EE
+if (((integral_pr(V,a0,b0,a)/pi-(nr+0.5_dp)*hbar_atomiko)*(integral_pr(V,a0,b0,EE)/pi-(nr+0.5_dp)*hbar_atomiko))<0.0_dp) then
+b=EE
+else
+a=EE
+endif
+
+enddo
+
+energia_bilaketa=EE
+end function energia_bilaketa
 
 
 
--- konstanteak.f90
+++ konstanteak.f90
@@ -4,9 +4,9 @@ real(kind=dp),parameter,public:: pi=acos(-1.0_dp)
 real(kind=dp),parameter,public:: hbar_atomiko=1.0_dp
 real(kind=dp),parameter,public:: hbar=1.054571817E-34_dp
 integer,parameter,public:: l=0
-real(kind=dp),parameter,public:: m1=21867.0_dp
-real(kind=dp),parameter,public:: m2=21867.0_dp
-real(kind=dp),parameter,public:: mu=m1*m2/(m1+m2)
+!real(kind=dp),parameter,public:: m1=42000.0_dp !21867.0_dp
+!real(kind=dp),parameter,public:: m2=42000.0_dp !21867.0_dp
+real(kind=dp),parameter,public:: mu=21000.0_dp !mu=m1*m2/(m1+m2)
 
 
 end module konstanteak
