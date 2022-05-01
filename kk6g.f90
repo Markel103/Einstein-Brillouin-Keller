a8e1ca5a0afc547b1c2c597fd04dd2b32dee3703
albahc01 <102954392+albahc01@users.noreply.github.com>
2022-05-01 12:14:20 +0200

    Add files via upload

--- kk6g.f90
+++ kk6g.f90
@@ -3,25 +3,23 @@ use mcf_tipos
 use konstanteak
 use funtzioak
 use eragiketak
-real(kind=dp)::r,a,b,r1,r2,c
+real(kind=dp)::r,a,b,r1,r2
 real(kind=dp),dimension(2)::r1r2
 integer::nmax,n
 open(unit=13,file="kk6gpuntuak.dat",action="write",status="replace")
 !open(unit=14,file="kk6gerroak.dat",action="write",status="replace")
-open(unit=101,file="deribatuVg.dat",action="write",status="replace")
+!open(unit=101,file="deribatuVg.dat",action="write",status="replace")
 
 a=0.4_dp
-b=2.0_dp
-nmax=20
+b=5.0_dp
+nmax=100000
 
 print*,minimoV(Vrg,a,b)
-l=0
 do n=0,nmax
 r=a+n*(b-a)/nmax
-print*,"r con el programa=",r,"V con el programa=",Vrg(r),"\n"
-!write(unit=13,fmt=*)r,Vrg(r)
+!print*,"r con el programa=",r,"V con el programa=",Vrg(r),"\n"
+write(unit=13,fmt=*)r,Vrg(r)
 !write(unit=101,fmt=*)r,deribatuV(Vrg,r)
-c=idatziV(Vrg,r)
 enddo
 !r1r2=erroak(Vrg,a,b,-1000000000000000000000000000000000000000000.0_dp)
 !r1=r1r2(1)
